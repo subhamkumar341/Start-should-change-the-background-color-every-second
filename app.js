6a5f4f6e148a2f8243e4b85efe40cb2c8379c304
Subham kumar <shubhamkumar2162002@gmail.com>
2025-06-01 14:59:59 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+// generate a random color
+
+
+const randomColor = function () {
+    const hex = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+
+  color +=hex [Math.floor(Math.random()*16)];
+    }
+
+
+    return color;
+}
+
+     let intervalID
+const startChangingColor = function () {
+
+     intervalID = setInterval(changeBackground,1000)
+     function changeBackground() {
+         document.body.style.backgroundColor = randomColor();
+     }
+}
+
+const stopChangingColor = function () {
+    clearInterval(intervalID);
+    intervalID =null
+}
+
+
+document.querySelector("#start").addEventListener("click", startChangingColor);
+document.querySelector("#stop").addEventListener("click", stopChangingColor);
+
+// console.log(randomColor());
+// console.log(Math.floor(Math.random()*16));
